695a2a16219fafe34a8ee274eedd6f700b365fca
anhtudo97 <anhtudo97@gmail.com>
2022-04-25 09:42:50 +0700

    feat(update): components

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,6 +1,9 @@
-export { default as About } from "./About/About";
-export { default as Footer } from "./Footer/Footer";
-export { default as Header } from "./Header/Header";
-export { default as Skills } from "./Skills/Skills";
-export { default as Testimonial } from "./Testimonial/Testimonial";
-export { default as Work } from "./Work/Work";
+import NavigationDots from './NavigationDots';
+import SocialMedia from './SocialMedia';
+import Navbar from './Navbar/Navbar';
+
+export {
+  NavigationDots,
+  SocialMedia,
+  Navbar,
+};
